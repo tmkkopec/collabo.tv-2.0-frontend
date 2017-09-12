2bd83c4caa4d1bc45c18d6a66be603671ff5da7b
skalawi <skalawiktor@gmail.com>
2017-09-12 16:26:11 +0200

    Sending request to googleapi

--- src/components/home/Searcher.js
+++ src/components/home/Searcher.js
@@ -1,30 +1,51 @@
 import React, { Component } from 'react';
 import MdlGrid from '../mdl/MdlGrid';
 import MdlCell from '../mdl/MdlCell';
+import SearchResults from './SearchResults';
 
 export default class Searcher extends Component {
 
     constructor(props) {
         super(props);
 
-        this.state = {query: ''};
-
+        this.state = {query: '', resultsVisible: false};
         this.onSubmit = this.onSubmit.bind(this);
         this.handleChange = this.handleChange.bind(this);
+        this.handleResponse = this.handleResponse.bind(this);
+    }
+
+    sendRequest(query, next){
+        var req = new XMLHttpRequest();
+        req.onreadystatechange = function() {
+            if (req.readyState == 4 && req.status == 200)
+                next(req.responseText);
+        }
+        req.open('GET', 'https://www.googleapis.com/youtube/v3/search?part=snippet&q='+query+'type=video&key=AIzaSyAkWmNO7E-FZWXxsNrD9nqsKTdvJpNti2I', true);
+        //req.setRequestHeader("Authorization", "Bearer  AIzaSyCRet3C8BuK0gCwuRW8jDgPj5Qx-KL8E3o");
+        req.send(null);
+    }
+
+    handleResponse(response){
+        console.log(response)
+
+        this.setState({resultsVisible: true});
+
     }
 
     handleChange(event) {
         this.setState({query: event.target.value})
+
     }
 
     onSubmit(event) {
         if (this.state.query !== '') {
-
+            this.sendRequest(this.state.query,this.handleResponse)
         }
         event.preventDefault();
     }
 
     render() {
+        console.log("render")
         return (
             <div className="mdl-layout__content">
                 <MdlGrid>
@@ -43,7 +64,11 @@ export default class Searcher extends Component {
                             </button>
                         </form>
                     </MdlCell>
+                    <MdlCell cellWidth={12}>
+                        <SearchResults visible={this.state.resultsVisible}/>
+                    </MdlCell>
                 </MdlGrid>
+
             </div>
         )
     }
